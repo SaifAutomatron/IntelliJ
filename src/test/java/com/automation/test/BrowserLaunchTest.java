5c662eb365bf74184faf2bd817658c7f54bf974c
SaifAutomatron <mohammaduchiha83@gmail.com>
2021-04-18 11:37:16 +0530

    second commit

--- src/test/java/com/automation/test/BrowserLaunchTest.java
+++ src/test/java/com/automation/test/BrowserLaunchTest.java
@@ -1,10 +1,10 @@
 package com.automation.test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
 
 public class BrowserLaunchTest {
@@ -16,6 +16,14 @@ public class BrowserLaunchTest {
         WebDriver driver=new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get("https://www.google.com/");
+
+        driver.get("https://testyantra.com/user/login");
+
+       String text=driver.findElement(By.xpath("//input[@id='edit-name']/preceding-sibling::label")).getText();
+       if(text.equalsIgnoreCase("username")){
+           driver.findElement(By.xpath("//input[@id='edit-name']")).sendKeys("bhau");
+       }
+
+
     }
 }
